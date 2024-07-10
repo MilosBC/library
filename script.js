bd87d2780d1ebee27efb48c2c5f60ed309f447b0
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2024-07-11 00:43:26 +0200

    Make the modal show up
    The modal is now showing after the button is clicked

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const allBooks = [];
 
+const newBook = document.querySelector('.new-book');
+const emptyLibrary = document.querySelector('.clear');
+const dialog = document.querySelector('dialog');
+
 function Book(author, title, numberOfPages) {
 this.author = author;
 this.title = title;
 this.numberOfPages = numberOfPages;
 }
+
+newBook.addEventListener('click', () => {
+dialog.showModal();
+})
 
